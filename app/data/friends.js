9f3b36b59dc24e0403036f4289d7f7ee3503433b
RyFogle <13fogle@gmail.com>
2019-06-30 13:02:54 -0700

    changing friends.js - syntax problem

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,51 +1,51 @@
 var friends = [
 
     {
-        "name":"Cyrus Hooper",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            2,
-            4,
-            3,
-            1,
-            2,
-            1,
-            3,
-            1
+        name:"Cyrus Hooper",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            "5",
+            "1",
+            "2",
+            "4",
+            "3",
+            "1",
+            "2",
+            "1",
+            "3",
+            "1"
           ]
       }, 
       {
-        "name":"Isabella Martin",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            3,
-            1,
-            4,
-            4,
-            4,
-            1,
-            5,
-            5,
-            1,
-            1
+        name:"Isabella Martin",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            "3",
+            "1",
+            "4",
+            "4",
+            "4",
+            "1",
+            "5",
+            "5",
+            "1",
+            "1"
           ]
       }, 
       {
-        "name":"Casey Park",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            1,
-            5,
-            2,
-            3,
-            5,
-            1,
-            4,
-            5,
-            4,
-            5
+        name:"Casey Park",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            "1",
+            "5",
+            "2",
+            "3",
+            "5",
+            "1",
+            "4",
+            "5",
+            "4",
+            "5"
           ]
       }
 ]; 
